267c1ac77edb878e6979da7864f78d1f192bf699
Mirza Rayhan <mirzarayhan22@gmail.com>
2021-03-02 10:34:05 +0700

    Pertemuan ke - 3

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from "register-service-worker";
 import reportWebVitals from './reportWebVitals';
+import Test from './Component/Test';
+import HelloComponent from './Login/Login';
+serviceWorker.unregister();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <HelloComponent />,
+  document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();  
+// function HelloComponent() {
+//   return HelloComponent;
+// }
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+
+// const HelloComponent = () => {
+// return HelloComponent
+// }
+
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return <p>StateFullComponent</p>
+//  }
+// }
+
+// ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
+//reportWebVitals();
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//      <App />
+//   </React.StrictMode>,
+// document.getElementById('root')
+// );
