dc2f4924b9f97b1e75d49563caa2e32b5c363e82
Mirza Rayhan <mirzarayhan22@gmail.com>
2021-03-04 14:49:57 +0700

    Pertemuan - 4

--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from "register-service-worker";
-import reportWebVitals from './reportWebVitals';
-import Test from './Component/Test';
-import HelloComponent from './Login/Login';
-serviceWorker.unregister();
+
+
+import BlogPost from './Container/BlogSpot/BlogSpot';
+
 
 ReactDOM.render(
-  <HelloComponent />,
-  document.getElementById('root'));
-
-reportWebVitals();  
-// function HelloComponent() {
-//   return HelloComponent;
-// }
-// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
-
-// const HelloComponent = () => {
-// return HelloComponent
-// }
-
-// class StateFullComponent extends React.Component{
-//   render(){
-//     return <p>StateFullComponent</p>
-//  }
-// }
-
-// ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
-//reportWebVitals();
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//      <App />
-//   </React.StrictMode>,
-// document.getElementById('root')
-// );
+  <BlogPost />,
+  document.getElementById('content'));
+serviceWorker.unregister();
