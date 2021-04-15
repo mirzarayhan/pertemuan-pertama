32071cc74afd0206e3d721ca690a396ead43a2bd
Mirza Rayhan <mirzarayhan22@gmail.com>
2021-04-15 20:50:28 +0700

    Little changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from "register-service-worker";
+import BlogSpot from './Container/BlogSpot/BlogSpot'
+import App from './App';
 
-
-import BlogPost from './Container/BlogSpot/BlogSpot';
-
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <BlogPost />,
-  document.getElementById('content'));
+   <BlogSpot />,
+document.getElementById('root'));
 serviceWorker.unregister();
